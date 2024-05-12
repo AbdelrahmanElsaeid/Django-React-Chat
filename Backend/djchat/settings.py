59eab4b2427e155dbae37cd10b7e7c222d37a780
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2024-05-12 12:47:55 +0300

    create models

--- Backend/djchat/settings.py
+++ Backend/djchat/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3=thby0g4#rf2egyvr!kss#4=awwa@idp)ptmq=r0#0ngt8rz-'
+SECRET_KEY =  os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'server',
+    'drf_spectacular',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -116,8 +123,33 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'account.Account'
+
+
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': True,
+    # OTHER SETTINGS
+}
--- Backend/server/views.py
+++ Backend/server/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
-
+from django.db.models import Count
+from rest_framework import viewsets
+from .models import Server
+from .serializer import ServerSerializer, CategorySerializer
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError,AuthenticationFailed
 # Create your views here.
+
+
+
+class ServerListViewSet(viewsets.ViewSet):
+    queryset = Server.objects.all()
+
+
+    def list(self, request):
+        category = request.query_params.get("category")
+        qty = request.query_params.get("qty")
+        by_user = request.query_params.get("by_user") == "true"
+        by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == "true"
+
+
+        if by_user or by_serverid and not request.user.is_authenticated:
+            raise AuthenticationFailed()
+
+        if category:
+            self.queryset.filter(category=category)
+
+        if qty:
+            self.queryset=self.queryset[: int(qty)]
+
+        if by_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id) 
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members = Count("member"))    
+
+        if by_serverid:
+            try:
+                self.queryset = self.queryset.filter(id=by_serverid)
+                if not self.queryset.exists():
+                    raise ValidationError(detail=f"Server with id {by_serverid} not found") 
+
+            except ValueError:
+                raise ValidationError(detail=f"Server Value Error")              
+
+
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members":with_num_members})  
+        return Response(serializer.data)      
+                    
+
